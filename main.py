aea6d45730b804f89f27ff0bb54ad0c19335a3fc
sillypears <sillypears@users.noreply.github.com>
2025-06-29 08:57:26 -0400

    feat: add match_forfeit field to Match model and update insert_match query

--- main.py
+++ main.py
@@ -189,13 +189,13 @@ async def get_match_stats():
 async def insert_match(match: Match, debug: bool = 0):
     query = '''
         INSERT INTO matches (
-            match_date, elo_rank_old, elo_rank_new, elo_change, match_win,
+            match_date, elo_rank_old, elo_rank_new, elo_change, match_win, match_forfeit,
             ranked_game_number, total_wins, win_streak_value, opponent_elo, opponent_estimated_elo, opponent_name,
             game_1_char_pick, game_1_opponent_pick, game_1_stage, game_1_winner,
             game_2_char_pick, game_2_opponent_pick, game_2_stage, game_2_winner,
             game_3_char_pick, game_3_opponent_pick, game_3_stage, game_3_winner
         ) VALUES (
-            %s, %s, %s, %s, %s,
+            %s, %s, %s, %s, %s, %s, 
             %s, %s, %s, %s, %s, %s,
             %s, %s, %s, %s,
             %s, %s, %s, %s,
@@ -208,7 +208,7 @@ async def insert_match(match: Match, debug: bool = 0):
             try:
                 await cur.execute(query, (
                     match.match_date,
-                    match.elo_rank_old, match.elo_rank_new, match.elo_change, match.match_win,
+                    match.elo_rank_old, match.elo_rank_new, match.elo_change, match.match_win, match.match_forfeit,
                     match.ranked_game_number, match.total_wins, match.win_streak_value, match.opponent_elo, match.opponent_estimated_elo, match.opponent_name,
                     match.game_1_char_pick, match.game_1_opponent_pick, match.game_1_stage, match.game_1_winner,
                     match.game_2_char_pick, match.game_2_opponent_pick, match.game_2_stage, match.game_2_winner,
