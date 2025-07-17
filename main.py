97d6659bb1aa45888ef86884e9650a470498c397
sillypears <sillypears@users.noreply.github.com>
2025-07-17 06:54:58 -0400

    feat: add final_move_id to match insertion and update logic

--- main.py
+++ main.py
@@ -314,13 +314,13 @@ async def insert_match(match: Match, debug: bool = 0):
             ranked_game_number, total_wins, win_streak_value, opponent_elo, opponent_estimated_elo, opponent_name,
             game_1_char_pick, game_1_opponent_pick, game_1_stage, game_1_winner,
             game_2_char_pick, game_2_opponent_pick, game_2_stage, game_2_winner,
-            game_3_char_pick, game_3_opponent_pick, game_3_stage, game_3_winner
+            game_3_char_pick, game_3_opponent_pick, game_3_stage, game_3_winner, final_move_id
         ) VALUES (
             %s, %s, %s, %s, %s, %s, 
             %s, %s, %s, %s, %s, %s,
             %s, %s, %s, %s,
             %s, %s, %s, %s,
-            %s, %s, %s, %s
+            %s, %s, %s, %s, %s
         )
     '''
     inserted_id = -1
@@ -333,7 +333,7 @@ async def insert_match(match: Match, debug: bool = 0):
                     match.ranked_game_number, match.total_wins, match.win_streak_value, match.opponent_elo, match.opponent_estimated_elo, match.opponent_name,
                     match.game_1_char_pick, match.game_1_opponent_pick, match.game_1_stage, match.game_1_winner,
                     match.game_2_char_pick, match.game_2_opponent_pick, match.game_2_stage, match.game_2_winner,
-                    match.game_3_char_pick, match.game_3_opponent_pick, match.game_3_stage, match.game_3_winner
+                    match.game_3_char_pick, match.game_3_opponent_pick, match.game_3_stage, match.game_3_winner, match.final_move_id
                 ))
                 await notify_websockets({'type': 'new_match', 'ranked_game_number': int(match.ranked_game_number)})
                 if match.match_win == 1:
