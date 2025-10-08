a84cbcfddf5e860b22b15a2a87c650f84f193dd5
sillypears <sillypears@users.noreply.github.com>
2025-10-08 08:00:40 -0400

    Updated stuff for new seasons

--- main.py
+++ main.py
@@ -152,7 +152,8 @@ async def get_characters(req: Request) -> dict:
 
 @app.get("/seasons", tags=["Seasons", "Meta"])
 async def get_seasons(req: Request) -> dict:
-
+    latest_season = await get_latest_season(req)
+    print(latest_season)
     query = f'''
         SELECT 
             id,
@@ -162,12 +163,18 @@ async def get_seasons(req: Request) -> dict:
             display_name 
         FROM seasons
     '''
-    return await err.safe_db_fetch_all(request=req, query=query)
-
+    seasons = await err.safe_db_fetch_all(request=req, query=query)
+    updated_seasons = [
+        {**season, 'latest': True} if season['id'] == latest_season['data']['id'] else {**season, 'latest': False}
+        for season in seasons['data']
+    ]
+    print(updated_seasons)
+    return err.SuccessResponse(data=updated_seasons).model_dump()
+    
 @app.get("/season/latest", tags=["Seasons", "Meta"])
 async def get_latest_season(req: Request):
     query = '''
-        SELECT id FROM seasons WHERE '%s' BETWEEN start_date AND end_date
+        SELECT id, short_name, display_name FROM seasons WHERE '%s' BETWEEN start_date AND end_date
     ''' % (datetime.now().isoformat())
     return await err.safe_db_fetch_one(request=req, query=query)
 
@@ -605,7 +612,7 @@ async def get_final_move_stats(req: Request) -> dict:
             WHERE
                 game_3_final_move_id <> - 1
                     AND game_3_winner = 1) AS all_winnings
-        GROUP BY final_move_name
+        GROUP BY final_move_name, season_display_name
         ORDER BY final_move_count DESC;
     '''
     return await err.safe_db_fetch_all(request=req, query=query)
